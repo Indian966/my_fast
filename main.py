6c7ce0a035dde57631e3eac188910bae2d5685f3
Indian966 <rlaalsrb4175@gmail.com>
2022-10-02 18:41:47 +0900

    add comment

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
-
+import requests
 app = FastAPI()
 
 #   RGB2DMC
 #   RGB 코드로 가장 유사한 십자수 실을 찾아준다.
 
 
-@app.get("/")
+@app.get("/rgb2dmc")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/index/")
+async def say_hello(requset):
+    return {"message": "Hello {name}"}
